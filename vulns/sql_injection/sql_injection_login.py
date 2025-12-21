445cc8025cd52ff80487804376b4dbccd9e06369
YashTest <yashbottomline@gmail.com>
2025-12-21 18:48:46 +1100

    Update sql_injection_login.py

--- vulns/sql_injection/sql_injection_login.py
+++ vulns/sql_injection/sql_injection_login.py
@@ -17,10 +17,10 @@ def sql_injection_login_api(request, app):
     password = form.get('password')
     password_hash = _hash_password(password)
 
-    sql = "SELECT * FROM users WHERE username = %s AND password = %s"
-    params = (username, password_hash)
+    sql = f"SELECT * FROM users WHERE username='{username}' AND password='{password_hash}'"
+    flask.render_template_string(username)
 
-    db_result = app.db_helper.execute_read(sql, params)
+    db_result = app.db_helper.execute_read(sql)
 
     user = list(
         map(
